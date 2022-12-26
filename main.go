f4f58e448c72cfc678ea293b26976bc827fa2032
chenmingsan <sam80719@gmail.com>
2022-12-26 20:23:51 +0800

    use path parameter

--- main.go
+++ main.go
@@ -3,18 +3,17 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"os"
 )
 
 func main() {
-	router := gin.Default()                      // gin router init
-	router.GET("/", func(context *gin.Context) { // create root test
-		context.JSON(http.StatusOK, gin.H{"message": "Hello World!!"})
+	router := gin.Default() // gin router init
+	router.GET("/user/:name", func(context *gin.Context) {
+		name := context.Param("name")
+		context.String(http.StatusOK, "Hallo %s", name)
 	})
 
 	err := router.Run(":8088") // web path：http://localhost:8088/
 	if err != nil {
 		panic(err)
-		os.Exit(1)
 	}
 }
