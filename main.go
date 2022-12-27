981c0811071afb2fcaddc81290ae92d08a462cf4
chenmingsan <sam80719@gmail.com>
2022-12-27 15:13:33 +0800

    know gin how to record gin log

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-	"app/router"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
-	router := router.SetRouterPublic()
+	t := time.Now()
+	//formatted := fmt.Sprintf("%d-%02d-%02d", t.Year(), t.Month(), t.Day())
+	formatted := fmt.Sprintf("%d%02d%02d", t.Year(), t.Month(), t.Day()) // time format
+	file, _ := os.Create(formatted + "gin.log")
+
+	gin.DefaultWriter = io.MultiWriter(file)
+
+	router := gin.Default()
+	router.Use(gin.LoggerWithFormatter(func(params gin.LogFormatterParams) string { // record more detail
+		return fmt.Sprintf("format log: %s - [%s] %s %s", params.ClientIP, params.TimeStamp, params.Method, params.Request)
+	}))
+	router.GET("/testLog", func(c *gin.Context) { // http://localhost:8088/testLog
+		c.String(http.StatusOK, "hello world!")
+	})
+
 	err := router.Run(":8088")
 	if err != nil {
 		panic(err)
