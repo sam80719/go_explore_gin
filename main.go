0c68b7c6ceedc4b810a892283efac615ec60a7f0
chenmingsan <sam80719@gmail.com>
2022-12-27 20:39:04 +0800

    know gin how to use form upload file

--- main.go
+++ main.go
@@ -4,15 +4,35 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
 	router := gin.Default()
+
+	// 32Mib
+	//router.MaxMultipartMemory = 8 // limit memory
+
 	router.Static("/", "./public") // http://localhost:8088/
-	router.POST("/login", func(c *gin.Context) {
+	router.POST("/upload", func(c *gin.Context) {
 		account := c.PostForm("account")
 		password := c.PostForm("password")
-		c.String(http.StatusOK, fmt.Sprintf("account=%s password=%s", account, password))
+
+		file, err := c.FormFile("file")
+
+		if err != nil {
+			c.String(http.StatusOK, fmt.Sprintf("error upload file: %s", err.Error()))
+			return
+		}
+
+		fileName := filepath.Base(file.Filename)
+
+		if err := c.SaveUploadedFile(file, fileName); err != nil {
+			c.String(http.StatusOK, fmt.Sprintf("error save file: %s", err.Error()))
+			return
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("account=%s password=%s upload file name is %s", account, password, fileName))
 	})
 
 	err := router.Run(":8088")
