8db8bbfb108d6c207c1ddb623266ba6a481ce38d
chenmingsan <sam80719@gmail.com>
2022-12-27 13:27:19 +0800

    know gin response data

--- main.go
+++ main.go
@@ -1,36 +1,44 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-type LoginForm struct {
-	Account  string `form:"account" binding:"required"`
-	Password string `form:"password" binding:"required"`
-}
-
 func main() {
-	router := gin.Default()             // gin router init
-	router.POST("/login", useStructTag) // use cmd: curl -v --form account=account1 --form password=passwordd http://localhost:8088/login
+	router := gin.Default()                              // gin router init
+	router.GET("/testJSON", func(context *gin.Context) { // http://localhost:8088/testJSON
+		context.JSON(http.StatusOK, gin.H{"message": "test json", "status": "OK"})
+	})
+	router.GET("/testJSON1", func(context *gin.Context) { // http://localhost:8088/testJSON1
+		var message struct {
+			Name  string
+			Sex   string
+			Phone int
+		}
 
+		message.Name = "Sam"
+		message.Sex = "man"
+		message.Phone = 95279527
+		context.JSON(http.StatusOK, message)
+	})
+	// follow above, we knew json can support struct
+
+	router.GET("/testXML", func(context *gin.Context) { // http://localhost:8088/testXML
+		context.XML(http.StatusOK, gin.H{"message": "test XML", "status": "OK"})
+	})
+
+	router.GET("/testYAML", func(context *gin.Context) { // http://localhost:8088/testYAML
+		context.YAML(http.StatusOK, gin.H{"message": "test YAML", "status": "OK"})
+	})
+
+	router.GET("/testHTML", func(context *gin.Context) { // http://localhost:8088/testHTML
+		context.PureJSON(http.StatusOK, gin.H{
+			"html": "<h1>HELLO WORLD!!!</h1>",
+		})
+	})
 	err := router.Run(":8088")
 	if err != nil {
 		panic(err)
-	}
-}
-
-func useStructTag(c *gin.Context) {
-	var f LoginForm
-	account := c.PostForm("account") // select post form data
-	fmt.Printf("account form is: %s\n", account)
-
-	if c.ShouldBind(&f) == nil { // by form
-		if f.Account == "account1" && f.Password == "password1" {
-			c.JSON(http.StatusOK, gin.H{"message": "already login"})
-		} else {
-			c.JSON(http.StatusUnauthorized, gin.H{"message": "login failure"})
-		}
 	}
 }
