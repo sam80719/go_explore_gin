edd556b005c8670240fc71ee0e9900d3d0c1f428
chenmingsan <sam80719@gmail.com>
2022-12-27 11:56:49 +0800

    struct tag for gin post

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ import (
 	"net/http"
 )
 
-type User struct {
-	Name  string `form:"name"`
-	Email string `form:"email"`
+type LoginForm struct {
+	Account  string `form:"account" binding:"required"`
+	Password string `form:"password" binding:"required"`
 }
 
 func main() {
-	router := gin.Default()           // gin router init
-	router.GET("/user", useStructTag) // http://localhost:8088/user?name=Sam&email=sam80719@gmail.com
+	router := gin.Default()             // gin router init
+	router.POST("/login", useStructTag) // use cmd: curl -v --form account=account1 --form password=passwordd http://localhost:8088/login
 
 	err := router.Run(":8088")
 	if err != nil {
@@ -22,11 +22,15 @@ func main() {
 }
 
 func useStructTag(c *gin.Context) {
-	var u User
-	if c.ShouldBindQuery(&u) == nil {
-		fmt.Println(u.Name)
-		fmt.Println(u.Email)
-	}
+	var f LoginForm
+	account := c.PostForm("account") // select post form data
+	fmt.Printf("account form is: %s\n", account)
 
-	c.String(http.StatusOK, "OK")
+	if c.ShouldBind(&f) == nil { // by form
+		if f.Account == "account1" && f.Password == "password1" {
+			c.JSON(http.StatusOK, gin.H{"message": "already login"})
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "login failure"})
+		}
+	}
 }
