e56819a8d89bc352dfb544d9a93ab0f6bb70e178
chenmingsan <sam80719@gmail.com>
2022-12-27 14:14:01 +0800

    know gin how to use router group

--- main.go
+++ main.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"app/router"
 )
 
 func main() {
-	router := gin.Default()                   // gin router init
-	router.GET("test", func(c *gin.Context) { // redirect page, http://localhost:8088/test
-		c.Redirect(http.StatusMovedPermanently, "https://www.google.com")
-	})
-
-	router.GET("/test1", func(c *gin.Context) { // redirect path, http://localhost:8088/test1
-		c.Request.URL.Path = "/test2"
-		router.HandleContext(c)
-	})
-	router.GET("/test2", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "Hello World!!"})
-	})
+	router := router.SetRouterPublic()
 	err := router.Run(":8088")
 	if err != nil {
 		panic(err)
